d341e745476354d59a6c3fd13ebbb8a6cfff7eff
GavinWu42711 <gavinwu42711@gmail.com>
2025-12-29 00:19:27 -0500

    bullet timeout
    - Added timing out to the bullets

--- merge-guns/Scripts/BulletClass.gd
+++ merge-guns/Scripts/BulletClass.gd
@@ -8,9 +8,13 @@ class_name BulletClass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	lifespan_timer()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move_and_slide()
+	
+func lifespan_timer() -> void:
+	await get_tree().create_timer(bullet_resource.bullet_base_stats["bullet_lifespan"] * bullet_resource.bullet_stat_mults["bullet_lifespan_mult"]).timeout
+	self.queue_free()
